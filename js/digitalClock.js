e7341ef57448301b76ba1826b7be2c755d5ff2a7
razmatinyan <matinyanraz@mail.ru>
2021-03-04 20:56:51 +0400

    Digital Clock (Recreated)
    It's a Digital Clock recreated by Raz

---
+++ js/digitalClock.js
@@ -0,0 +1,28 @@
+function clock() {
+   let hours = document.querySelector('.hours')
+   let minutes = document.querySelector('.minutes')
+   let seconds = document.querySelector('.seconds')
+   let ampm = document.querySelector('.ampm')
+
+   let h = new Date().getHours()
+   let m = new Date().getMinutes()
+   let s = new Date().getSeconds()
+
+   let am = 'AM'
+
+   if (h > 12) {
+      h = h - 12
+      am = 'PM'
+   }
+
+   h = (h < 10) ? '0' + h : h
+   m = (m < 10) ? '0' + m : m
+   s = (s < 10) ? '0' + s : s
+
+   hours.innerHTML = h
+   minutes.innerHTML = m
+   seconds.innerHTML = s
+   ampm.innerHTML = am;
+}
+
+let interval = setInterval(clock, 1000)
